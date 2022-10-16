3a20bc65ff6af71078a3f24f190e3a7c5e061432
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2022-10-16 19:26:01 +0700

    Update histori_laundry.php

--- laundry_ukl/histori_laundry.php
+++ laundry_ukl/histori_laundry.php
@@ -1,10 +1,20 @@
 <?php 
     include "header.php";
 ?>
-<h2>Histori Laundry</h2>
+<h2 align="center">Histori Laundry</h2>
 <table class="table table-hover table-striped">
     <thead>
-        <th>NO</th><th>Nama User</th><th>Nama Meber</th><th>Paket</th><th>qty</th><th>subtotal</th><th>Tanggal laundry</th><th>Tanggal selesai</th><th>tanggal bayar</th><th>Status</th><th>Pembayaran</th><th>Aksi</th>
+    <?php
+     if ($_SESSION['role']=="owner"){
+    ?>
+        <th>NO</th><th>Nama User</th><th>Nama Meber</th><th>Paket</th><th>qty</th><th>subtotal</th><th>Tanggal laundry</th><th>Tanggal selesai</th><th>tanggal bayar</th><th>Status</th><th>Pembayaran</th>
+    <?php
+     }else{
+    ?>
+            <th>NO</th><th>Nama User</th><th>Nama Meber</th><th>Paket</th><th>qty</th><th>subtotal</th><th>Tanggal laundry</th><th>Tanggal selesai</th><th>tanggal bayar</th><th>Status</th><th>Pembayaran</th><th>Aksi</th>
+    <?php
+     }
+    ?>
     </thead>
     <tbody>
         <?php 
@@ -13,7 +23,6 @@
         $no=0;
         while($dt_histori=mysqli_fetch_array($qry_histori)){
             $no++;
-            //menampilkan buku yang dipinjam
             $paket="<ol>";
             $subtotal="<ol>";
             $qty="<ol>";
@@ -26,22 +35,42 @@
             $paket.="</ol>";
             $subtotal.="</ol>";
             $qty.="</ol>";
-            //menampilkan status sudah kembali atau belum
+            //menampilkan status dan pembayaran
             $qry_cek_bayar=mysqli_query($conn,"select * from transaksi where id_transaksi = '".$dt_histori['id_transaksi']."'");
             if(mysqli_num_rows($qry_cek_bayar)>0){
                 $dt_bayar=mysqli_fetch_array($qry_cek_bayar);
-                // $denda="denda Rp. ".$dt_kembali['denda'];
-                $status_proses="<label class='alert alert-success'>".$dt_histori['status']."</label>";
+                //if untuk status
+                if($dt_histori['status'] == 'baru'){
+                    $status_proses="<label div class='alert alert-danger' >".$dt_histori['status']."</label>";
+                } elseif($dt_histori['status'] == 'proses') {
+                    $status_proses="<label div class='alert alert-primary' >".$dt_histori['status']."</label>";
+                }elseif($dt_histori['status'] == 'selesai') {
+                    $status_proses="<label div class='alert alert-warning' >".$dt_histori['status']."</label>";
+                }elseif($dt_histori['status'] == 'diambil') {
+                    $status_proses="<label div class='alert alert-success' >".$dt_histori['status']."</label>";
+                }
+                //if untuk pembayaran
+                if ($dt_histori['dibayar']=="belum_dibayar"){
                 $status_bayar="<label class='alert alert-danger'>".$dt_histori['dibayar']."</label>";
-                $button_update="<a href='update.php?id=".$dt_histori['id_transaksi']."' class='btn btn-warning' onclick='return confirm(\"apakah anda yakin?\")'>update</a>";
-            } else {
-                // $status_proses="<label class='alert alert-success'>".$dt_histori['status']."</label>";
-                // $status_bayar="<label class='alert alert-danger'>Belum dibayar</label>";
-                // $button_update="<a href='update.php?id=".$dt_histori['id_transaksi']."' class='btn btn-warning' onclick='return confirm(\"apakah anda yakin?\")'>update</a>";
-            }
+                }
+                else{
+                    $status_bayar="<label class='alert alert-success'>".$dt_histori['dibayar']."</label>";
+                }
+                $button_update="<a href='update.php?id_transaksi=".$dt_histori['id_transaksi']."' class='btn btn-warning'>Update</a>";
+            } 
         ?>
             <tr>
+                <?php
+                if ($_SESSION['role']=="owner"){
+                ?>
+                <td><?=$no?></td><td><?=$dt_histori['nama_user']?></td><td><?=$dt_histori['nama_member']?></td><td><?=$paket?></td><td><?=$qty?></td><td><?=$subtotal?></td><td><?=$dt_histori['tgl']?></td><td><?=$dt_histori['batas_waktu']?></td><td><?=$dt_histori['tgl_bayar']?></td><td><?=$status_proses?></td><td><?=$status_bayar?></td>
+                <?php
+                }else{
+                ?>
                 <td><?=$no?></td><td><?=$dt_histori['nama_user']?></td><td><?=$dt_histori['nama_member']?></td><td><?=$paket?></td><td><?=$qty?></td><td><?=$subtotal?></td><td><?=$dt_histori['tgl']?></td><td><?=$dt_histori['batas_waktu']?></td><td><?=$dt_histori['tgl_bayar']?></td><td><?=$status_proses?></td><td><?=$status_bayar?></td><td><?=$button_update?></td>
+                <?php
+                }
+                ?>
             </tr>
         <?php
         }
