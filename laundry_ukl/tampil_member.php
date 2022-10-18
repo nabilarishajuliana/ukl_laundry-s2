42a651188a1e5581e7c6ab4e9d8421a3228a1cab
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2022-10-18 07:45:36 +0700

    Update tampil_member.php

--- laundry_ukl/tampil_member.php
+++ laundry_ukl/tampil_member.php
@@ -35,6 +35,7 @@ $qry_member=mysqli_query($conn,"select * from member");
             ?>
         </tbody>
     </table>
+    <a href="tambah_member.php" class='btn btn-warning'>Tambah Member</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
