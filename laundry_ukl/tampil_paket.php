5fe4a5c67981c712a86ec0b990c8f153a961e573
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2022-10-18 07:43:07 +0700

    Update tampil_paket.php

--- laundry_ukl/tampil_paket.php
+++ laundry_ukl/tampil_paket.php
@@ -34,6 +34,7 @@ $qry_paket=mysqli_query($conn,"select * from paket");
             ?>
         </tbody>
     </table>
+    <a href="tambah_paket.php" class='btn btn-warning'>Tambah Paket</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
